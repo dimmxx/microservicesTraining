4f714fb6dfc19303ea8acc1a667b0b88dcf2f6f6
dimmxx <dimmxx@gmail.com>
2020-08-20 11:32:53 +0300

    fallback

--- employee-producer/src/main/java/com/example/producer/controller/EmployeeController.java
+++ employee-producer/src/main/java/com/example/producer/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.example.producer.controller;
 
 import com.example.producer.model.Employee;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmployeeController {
 
     @RequestMapping(value = "/employee", method = RequestMethod.GET)
+    @HystrixCommand(fallbackMethod = "getDataFallBack")
     public Employee firstPage() {
 
         Employee employee = new Employee();
@@ -18,6 +20,21 @@ public class EmployeeController {
         employee.setEmpId("1");
         employee.setSalary(3000);
 
+
+        if(true){
+            throw new RuntimeException();
+        }
+
+
+        return employee;
+    }
+
+    public Employee getDataFallBack() {
+        Employee employee = new Employee();
+        employee.setName("fallback-emp1");
+        employee.setDesignation("fallback-manager");
+        employee.setEmpId("fallback-1");
+        employee.setSalary(0);
         return employee;
     }
 
